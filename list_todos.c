927353378150b440a89ae51674ef3d141d9ecf48
DonRyuDragoni <dragoni.ryu@gmail.com>
2014-12-27 21:32:57 -0200

    Added parser for command-line options and support for output to file

--- list_todos.c
+++ list_todos.c
@@ -1,38 +1,121 @@
 /*
- * An attempt to build my first usefull program in C
- *
- * This is supposed to read a file and search in it
- * for lines beggining with "TODO" and list them in
+ * This is supposed to read a file and search in it 
+ * for lines containing "TODO:" and list them in
  * order, printing something like:
  *
+ *     File dummy_file.txt:
+ *
  *     On line 15:
- *         list files used untill now
+ *         TODO: list files used untill now
  *     On line 122:
- *         Rewrite the function, using recursion
+ *        cout << "ERR22, abort"; // TODO: fix 
  * 
- * (Note that the notes are printed ignoring the "TODO"
- * strings. After all, this program is already about
- * printing TODOs, so no need to remaind it in every
- * f*cking line.)
+ * Originally, only lines starting with the TODO
+ * string were supposed to be printed, but this could
+ * be considered a feature too, so lei it be like this.
+ *
+ * For now, the program only works for one file at a time;
+ * for me, this is fine and ideal, but it may change.
  *
  */
 
 // Constants
-#define TODO "TODO:"
-#define LINE_MAX_SIZE 512
+#define TODO "TODO:"      // string to be searched
+#define LINE_MAX_SIZE 700 // max bytes per line
 
 // Required libraries
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<argp.h>
 
-int main(int argc, char* argv[])
+// program version
+const char *argp_program_version = "list_todos 1.1";
+// email for bug fixes
+const char *argp_program_bug_address = "<dragoni.ryu@gmail.com>";
+
+// struct to communicate with parse_opt
+struct arguments {
+    char *args[1];
+    int verbose;
+    char *outfile;
+};
+
+/*
+ * Options, field 1 in ARGP
+ *     fields: {NAME, KEY, ARG, FLAGS, DOC}
+ */
+static struct argp_option options[] = {
+    {"output", 'o', "OUTFILE", 0, "Output to OUTFILE instead of stdin"},
+    {0}
+};
+
+/*
+ * Parser, field 2 in ARGP
+ *     fields: {KEY, ARG, STATE}
+ */
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;
+
+    switch(key) {
+        case 'v':
+            arguments->verbose = 1;
+            break;
+        case 'o':
+            arguments->outfile = arg;
+        case ARGP_KEY_ARG:
+            if(state->arg_num >= 1)
+                argp_usage(state);
+            arguments->args[state->arg_num] = arg;
+            break;
+        case ARGP_KEY_END:
+            if (state->arg_num < 1)
+                argp_usage(state);
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+/*
+ * Documentation, field 3 in ARGP
+ *     Description of the non-option command-line arguments that we accept.
+ */
+static char args_doc[] = "FILENAME";
+
+/*
+ * Program documentation, field 4 in ARGP
+ *     Program documentation.
+ */
+static char doc[] = "list_todos -- A program to list all TODO notes in a file.";
+
+// The ARGP structure
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
+// Main program goes here
+int main(int argc, char **argv)
 {
-    if (argc < 2)
-        printf("Filename not specified.");
+    struct arguments arguments;
+    
+    // argument defaults
+    arguments.outfile = NULL;
+    arguments.verbose = 0;
+
+    // let the magic begin
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    // send the output to outfile, if asked
+    FILE *outstream;
+    if (arguments.outfile)
+        outstream = fopen(arguments.outfile, "w");
+    else
+        outstream = stdout;
+
+    fprintf(outstream, "File %s:\n\n", arguments.args[0]);
 
     // Dummy file for testing
-    FILE* fp = fopen(argv[1], "r");
+    FILE *fp = fopen("dummy_file.txt", "r");
     if (fp == NULL) {
         // Print error to stderr
         fprintf(stderr, "Failed to open file %s\n", argv[1]);
@@ -45,8 +128,8 @@ int main(int argc, char* argv[])
     int line_num = 1;
     while (fgets(buffer, sizeof buffer, fp) != NULL) {
         if (strstr(buffer, TODO) != NULL) {
-            printf("On line %d:\n", line_num);
-            printf("\t%s", buffer);
+            fprintf(outstream, "On line %d:\n", line_num);
+            fprintf(outstream, "\t%s", buffer);
         }
         ++line_num;
     }

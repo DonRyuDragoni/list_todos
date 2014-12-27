9fbafae96e0dcb9b8d4403c823be876344255b72
DonRyuDragoni <dragoni.ryu@gmail.com>
2014-12-27 21:51:11 -0200

    Add suport for user-changing line/buffer size

--- list_todos.c
+++ list_todos.c
@@ -20,8 +20,12 @@
  */
 
 // Constants
-#define TODO "TODO:"      // string to be searched
-#define LINE_MAX_SIZE 700 // max bytes per line
+#define TODO "TODO:" // string to be searched
+// Default behavior of the program
+#define DEFAULT_LINE_SIZE 700
+#define DEFAULT_SHOW_FULLPATH 0
+#define DEFAULT_PRINT_OUTFILE NULL
+#define DEFAULT_IGNORE_WARNINGS 0
 
 // Required libraries
 #include<stdio.h>
@@ -36,9 +40,11 @@ const char *argp_program_bug_address = "<dragoni.ryu@gmail.com>";
 
 // struct to communicate with parse_opt
 struct arguments {
-    char *args[1]; // ARG1
-    int fullpath;  // the -f flag
-    char *outfile; // argument for -o
+    char *args[1]; // ARG1, the name of the file
+    int fullpath;  // the -f flag, show or not the full path
+    int line_size; // argument for -s, change default size of lines
+    char *outfile; // argument for -o, file to be printed in
+    int ignore_warnings;  // the -i flag, ignore or not warnings
 };
 
 /*
@@ -47,7 +53,9 @@ struct arguments {
  */
 static struct argp_option options[] = {
     {"fullpath", 'f', 0, 0, "Prints the full path of the file"},
+    {"line-size", 's', "LINESIZE", 0, "Change line (buffer) size, in bytes"},
     {"output", 'o', "OUTFILE", 0, "Output to OUTFILE instead of stdin"},
+    {"ignore-warnings", 'i', 0, 0, "Ignore buffer size checking and run anyway; please note this leads to unknown behavior"},
     {0}
 };
 
@@ -60,10 +68,25 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
     switch(key) {
         case 'f':
-            arguments->fullpath = 1;
+            // swap the behavior of the fullpath flag
+            if (DEFAULT_SHOW_FULLPATH == 1)
+                arguments->fullpath = 0;
+            else
+                arguments->fullpath = 1;
             break;
         case 'o':
             arguments->outfile = arg;
+            break;
+        case 's':
+            arguments->line_size = atoi(arg);
+            break;
+        case 'i':
+            // swap the behavior of the warnings flag
+            if (DEFAULT_IGNORE_WARNINGS == 1)
+               arguments->ignore_warnings = 0;
+            else
+               arguments->ignore_warnings = 1; 
+            break;
         case ARGP_KEY_ARG:
             if(state->arg_num >= 1)
                 argp_usage(state);
@@ -94,14 +117,29 @@ static char doc[] = "list_todos -- A program to list all TODO notes in a file.";
 // The ARGP structure
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
+// scans the whole file for inconsistent lines
+// (inconsisted lines = scanned buffer with no '\n')
+int check_buffer_size(FILE *fp, int buffer_size)
+{
+    char buffer[buffer_size];
+    while (fgets(buffer, sizeof buffer, fp) != NULL) {
+        if (strstr(buffer, "\n") == NULL && fgets(buffer, sizeof buffer, fp) != NULL)
+            return 1;
+    }
+    rewind(fp);
+    return 0;
+}
+
 // Main program goes here
 int main(int argc, char **argv)
 {
     struct arguments arguments;
     
     // argument defaults
-    arguments.outfile = NULL;
-    arguments.fullpath = 0;
+    arguments.outfile = DEFAULT_PRINT_OUTFILE;
+    arguments.fullpath = DEFAULT_SHOW_FULLPATH;
+    arguments.line_size = DEFAULT_LINE_SIZE; 
+    arguments.ignore_warnings = DEFAULT_IGNORE_WARNINGS;
 
     // let the magic begin
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
@@ -122,11 +160,21 @@ int main(int argc, char **argv)
     else
         outstream = stdout;
 
-    // Dummy file for testing
+    // Open the file and check existence
     FILE *fp = fopen(full_filename, "r");
     if (fp == NULL) {
         // Print error to stderr
         fprintf(stderr, "Failed to open file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+
+    // if ignore-warnings is off, check the buffer size against
+    // the file; if some warning is found, close the file, print
+    // error message and exit
+    char buffer[arguments.line_size];
+    if (!arguments.ignore_warnings && check_buffer_size(fp, arguments.line_size)) {
+        fprintf(outstream, "Warning: buffer size may be too small.\nConsider changing it with the -s flag (default size is %d)\n", DEFAULT_LINE_SIZE);
+        fclose(fp);
         return EXIT_FAILURE;
     }
 
@@ -135,7 +183,6 @@ int main(int argc, char **argv)
 
     // Search for the string TODO in current file, line by line
     // if found, print the whole line
-    char buffer[LINE_MAX_SIZE];
     int line_num = 1;
     while (fgets(buffer, sizeof buffer, fp) != NULL) {
         if (strstr(buffer, TODO) != NULL) {
